367e252ed85be95327ff23b9bcb5443b1d439801
Bcdantas <bcarneirodantas@gmail.com>
2024-07-03 20:51:41 -0300

    exercicios

---
+++ 1067.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+    int x,i=1;
+
+    cin >> x ;
+
+    while(i<=x){
+        if(i%2!=0){
+            cout << i << endl;  
+        }
+    i++;
+    }
+
+}
